666987b60867bd9bd2c054b8c61d116938924cbf
TheGoodDarkSide <the.good.dark.side@gmail.com>
2024-05-12 16:44:01 +0200

    test

--- defines.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define( 'INSTAGRAM_APP_ID', '808510217872665' );
-	define( 'INSTAGRAM_APP_SECRET', '24d857087673311ac6720adb1b7ab27a' );
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://sites.google.com/view/picugo' );
